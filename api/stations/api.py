fe21e1bc9cc93284184b7a9aa22d23b58b6e325b
Joe Futrelle <jfutrelle@whoi.edu>
2024-07-16 17:34:39 -0400

    basic stations API implementation

--- api/stations/api.py
+++ api/stations/api.py
@@ -1,7 +1,35 @@
+from typing import List
+from datetime import datetime
+
 from ninja import Router
+
+from .services import StationService, StationInput, StationLocationInput, StationQueryOutput, NearestStationQueryInput, NearestStationQueryOutput
+
 
 router = Router()
 
-@router.get("/")
-def stations_root(request):
-    return {"message": "Welcome to the stations API"}
+@router.get("/now", response=List[StationQueryOutput])
+def get_stations_now(request):
+    return StationService.get_stations()
+
+
+@router.get("/at/{timestamp}", response=List[StationQueryOutput])
+def get_stations(request, timestamp: datetime):
+    return StationService.get_stations(timestamp)
+
+
+@router.post('/nearest', response=NearestStationQueryOutput)
+def get_nearest_station(request, query: NearestStationQueryInput):
+    return StationService.get_nearest_station(query)
+
+
+@router.post('/create')
+def create_station(request, input: StationInput):
+    StationService.create_station(input)
+    return 204
+
+
+@router.post('/set_location')
+def set_location(request, input: StationLocationInput):
+    StationService.set_location(input)
+    return 204
---
+++ api/stations/services.py
@@ -0,0 +1,105 @@
+from typing import Optional
+from datetime import datetime
+
+from pydantic import BaseModel
+
+from core.models import Station, StationLocation
+
+
+class StationInput(BaseModel):
+    name: str
+    full_name: str
+
+
+class StationLocationInput(BaseModel):
+    station_name: str
+    latitude: float
+    longitude: float
+    start_time: datetime
+    end_time: Optional[datetime] = None
+    depth: Optional[float] = None
+    comment: Optional[str] = ''
+
+
+class StationQueryInput(BaseModel):
+    station_name: str
+    timestamp: datetime
+
+
+class StationsQueryInput(BaseModel):
+    timestamp: datetime
+
+
+class StationQueryOutput(BaseModel):
+    station_name: str
+    latitude: float
+    longitude: float
+    start_time: datetime
+    end_time: Optional[datetime] = None
+    depth: Optional[float] = None
+    comment: str
+
+
+class NearestStationQueryInput(BaseModel):
+    latitude: float
+    longitude: float
+    timestamp: Optional[datetime] = None
+
+
+class NearestStationQueryOutput(BaseModel):
+    station_name: str
+    latitude: float
+    longitude: float
+    distance: float
+
+
+class StationService:
+    @staticmethod
+    def serialize_station_location(location: StationLocation) -> StationQueryOutput:
+        return StationQueryOutput(
+            station_name=location.content_object.name,
+            latitude=location.geolocation.y,
+            longitude=location.geolocation.x,
+            start_time=location.start_time,
+            end_time=location.end_time,
+            depth=location.depth,
+            comment=location.comment
+        )
+    
+    @staticmethod
+    def create_station(station_input: StationInput):
+        station = Station.objects.create(
+            name=station_input.name,
+            full_name=station_input.full_name
+        )
+
+    @staticmethod
+    def set_location(location: StationLocationInput):
+        station = Station.objects.get(name=location.station_name)
+        station.set_location(
+            latitude=location.latitude,
+            longitude=location.longitude,
+            start_time=location.start_time,
+            end_time=location.end_time,
+            depth=location.depth,
+            comment=location.comment
+        )
+
+    @staticmethod
+    def get_nearest_station(query: NearestStationQueryInput) -> NearestStationQueryOutput:
+        station_location = Station.nearest_location(
+            latitude=query.latitude,
+            longitude=query.longitude,
+            timestamp=query.timestamp
+        )
+        return NearestStationQueryOutput(
+            station_name=station_location.content_object.name,
+            latitude=station_location.geolocation.y,
+            longitude=station_location.geolocation.x,
+            distance=station_location.distance.km
+        )
+    
+    @classmethod
+    def get_stations(cls, timestamp: datetime = None) -> list[StationQueryOutput]:
+        station_locations = Station.get_locations(timestamp)
+        return [cls.serialize_station_location(location) for location in station_locations]
