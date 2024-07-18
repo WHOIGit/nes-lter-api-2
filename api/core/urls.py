204adcbf03d28e0c41dc0cfb8b1909f8087c5e72
Joe Futrelle <jfutrelle@whoi.edu>
2024-07-18 08:03:23 -0400

    /api/login endpoint backed by DRF obtain_auth_token view

--- api/core/urls.py
+++ api/core/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from ninja import NinjaAPI
+from rest_framework.authtoken.views import obtain_auth_token
+
 from core.api import router as core_router
 from stations.api import router as stations_router
 
@@ -9,5 +11,6 @@ api.add_router('', core_router)
 api.add_router('/stations/', stations_router)
 
 urlpatterns = [
+    path('api/login', obtain_auth_token), # a bit of a hack to use the DRF obtain_auth_token view
     path('api/', api.urls),
 ]
