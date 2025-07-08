6d758e62cc3d4e126e6936b25080312cf744f86e
Joe Futrelle <jfutrelle@whoi.edu>
2025-07-08 09:44:53 -0400

    Swagger-compatible DRF-based token authenticator

--- api/core/auth.py
+++ api/core/auth.py
@@ -1,17 +1,18 @@
+from ninja.security import HttpBearer
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.exceptions import AuthenticationFailed
 from ninja.errors import HttpError
 
-
-class TokenAuthenticator:
+class TokenAuthenticator(HttpBearer):
     def __init__(self):
         self.auth = TokenAuthentication()
+        super().__init__()  # Make sure to call parent init!
 
-    def __call__(self, request):
-        try:
-            user = self.auth.authenticate(request)
-            if user is None:
-                raise AuthenticationFailed('Unauthorized')
-            return user
-        except AuthenticationFailed:
+    def authenticate(self, request, token):
+        # This ensures the DRF token auth will see the header
+        request.META['HTTP_AUTHORIZATION'] = f"Token {token}"
+        user_auth_tuple = self.auth.authenticate(request)
+        if user_auth_tuple is None:
             raise HttpError(401, 'Unauthorized')
+        user, _ = user_auth_tuple
+        return user
