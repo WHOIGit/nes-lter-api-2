2f6cf06a5113636c0e327e1b11227321fe203f43
Joanne Koch <jkoch@whoi.edu>
2025-10-08 10:40:08 -0400

    test importnutrients

--- api/nut/management/commands/importnut.py
+++ api/nut/management/commands/importnut.py
@@ -8,9 +8,8 @@ from django.contrib.gis.geos import Point
 from core.models import Cruise, Station
 from storage.mediastore import MediaStore
 from storage.utils import PrefixStore
-from django.conf import settings
 import numpy as np
-from core.utils import clean_column_names, wide_to_long, path_to_cast
+from core.utils import clean_column_names, wide_to_long, path_to_cast, get_store
 
 NUT_SUFFIX = '_nut.csv'
 BTLSUM_SUFFIX = '_ctd_bottle_summary.csv'
@@ -381,10 +380,9 @@ class Command(BaseCommand):
                     csv_binary = csv_buffer.getvalue().encode("utf-8")
 
                     object_key = f"{cruise_name}{NUT_SUFFIX}"
-                    with MediaStore(self.URL, token=self.TOKEN) as store:
-                        prefix = PrefixStore(store, self.MEDIASTORE_PREFIX)
+                    with get_store(self.URL, self.TOKEN, self.MEDIASTORE_PREFIX) as store:
                         try:
-                            prefix.put(object_key, csv_binary)
+                            store.put(object_key, csv_binary)
                             self.stdout.write(self.style.SUCCESS(f'{cruise_name}{NUT_SUFFIX} successfully created.'))
                         except Exception as e:
                             print(e, flush=True)
